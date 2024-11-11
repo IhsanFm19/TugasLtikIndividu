5fa6b6110782c60c0b35c765335b122dfa1775ec
IhsanFm19 <ifm191919@gmail.com>
2024-11-11 20:57:18 +0700

    menambahkan input user pada praktikum 1 di branch revisi

--- AKtivitas Praktikum/AktivitasPraktikum_1_P9.py
+++ AKtivitas Praktikum/AktivitasPraktikum_1_P9.py
@@ -2,8 +2,11 @@
 #NIM : 2401413
 #Kelas : 1C
 
+radius = int(input("Masukkan radius tabung : "))
+tinggi = int(input("Masukkan tinggi tabung : "))
+
 def VolumeTabung (r,t):
     #rumus volume tabung (phi x r^2 x t)
     print(22/7*r**2*t)
 
-VolumeTabung(7,14)
+VolumeTabung(radius,tinggi)
