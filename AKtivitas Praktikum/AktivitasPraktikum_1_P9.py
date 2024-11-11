35e716e00486772bfba79a9d382196d49e66d223
IhsanFm19 <ifm191919@gmail.com>
2024-11-11 21:06:47 +0700

    menambahkan variable phi

--- AKtivitas Praktikum/AktivitasPraktikum_1_P9.py
+++ AKtivitas Praktikum/AktivitasPraktikum_1_P9.py
@@ -2,11 +2,12 @@
 #NIM : 2401413
 #Kelas : 1C
 
+phi = 22/7
 radius = int(input("Masukkan radius tabung : "))
 tinggi = int(input("Masukkan tinggi tabung : "))
 
 def VolumeTabung (r,t):
     #rumus volume tabung (phi x r^2 x t)
-    print(22/7*r**2*t)
+    print(phi*r**2*t)
 
 VolumeTabung(radius,tinggi)
